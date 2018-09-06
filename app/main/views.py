edc0a5a7b9bb0c0becf698b647a5076441934940
Trent <yliu219@syr.edu>
2018-09-05 23:22:19 -0400

    flash wrong username/password

--- app/main/views.py
+++ app/main/views.py
@@ -196,17 +196,16 @@ def feedbackCompare2():
 # handle the admin login event
 @main.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
+            flash('Wrong username')
         elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
+            flash('Invalid password')
         else:
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('main.admin'))
-    return render_template('login.html', error=error)
+    return render_template('login.html')
 
 # get all the information needed in the database to display on the admin page
 @main.route('/admin')
